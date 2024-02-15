f004ec915446e65da87a4f2cc04c6e712d731828
Dvwhite81 <Dvwhite1981@gmail.com>
2024-02-15 18:02:07 -0500

    Added track volume

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -5,13 +5,29 @@ import './Track.css';
 function Track({ choice, currentBeat, totalBeats, isPlaying }) {
   const { name, displayName, options } = choice;
   const [soundToPlay, setSoundToPlay] = useState(options[0].soundSrc);
-  const [selectClass, setSelectClass] = useState('hidden');
+  const [selectClass, setSelectClass] = useState(' hidden');
+  const [volumeClass, setVolumeClass] = useState('');
+  const [muteClass, setMuteClass] = useState(' hidden');
+  const [sliderClass, setSliderClass] = useState(' hidden');
+  const [volume, setVolume] = useState(100);
 
   const toggleSelectOpen = () => {
-    if (selectClass === 'hidden') {
+    if (selectClass === ' hidden') {
       setSelectClass('');
     } else {
-      setSelectClass('hidden');
+      setSelectClass(' hidden');
+    }
+  };
+
+  const toggleMute = () => {
+    if (volumeClass === ' hidden') {
+      setVolumeClass('');
+      setMuteClass(' hidden');
+      setVolume(1);
+    } else {
+      setVolumeClass(' hidden');
+      setMuteClass('');
+      setVolume(0);
     }
   };
 
@@ -24,6 +40,7 @@ function Track({ choice, currentBeat, totalBeats, isPlaying }) {
     // eslint-disable-next-line no-undef
     const sound = new Audio();
     sound.src = soundToPlay;
+    sound.volume = volume / 100;
     sound.play();
   };
 
@@ -39,7 +56,37 @@ function Track({ choice, currentBeat, totalBeats, isPlaying }) {
 
   return (
     <div id={`${name}-track`} className="track">
-      <div id={`${name}-track-control`} className="track-control">
+      <div
+        id={`${name}-track-control`}
+        className="track-control"
+        onMouseLeave={() => setSliderClass(' hidden')}
+      >
+        <button
+          type="button"
+          className="btn mute-btn"
+          onClick={toggleMute}
+          onMouseEnter={() => setSliderClass('')}
+        >
+          <img
+            className={`icon volume-icon${volumeClass}`}
+            src="/images/volume-icon.png"
+            alt="volume icon"
+          />
+          <img
+            className={`icon mute-icon${muteClass}`}
+            src="/images/mute-icon.png"
+            alt="mute icon"
+          />
+        </button>
+        <div className={`track-volume-div${sliderClass}`}>
+          <input
+            type="range"
+            min={0}
+            max={100}
+            value={volume}
+            onChange={({ target }) => setVolume(target.value)}
+          />
+        </div>
         <button
           type="button"
           className="btn track-select-btn"
@@ -47,7 +94,7 @@ function Track({ choice, currentBeat, totalBeats, isPlaying }) {
         >
           <p className="track-label">{displayName}</p>
         </button>
-        <ul className={`select track-select ${selectClass}`}>
+        <ul className={`select track-select${selectClass}`}>
           {options.map((o) => (
             <li key={o.soundName} className="select-option">
               <button
