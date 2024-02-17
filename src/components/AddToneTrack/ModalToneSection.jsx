dc3b317ff5101f0abf77eb024d51f378b44aa03c
Dvwhite81 <Dvwhite1981@gmail.com>
2024-02-17 14:32:03 -0500

    Small update to modal. Done for now

--- src/components/AddToneTrack/ModalToneSection.jsx
+++ src/components/AddToneTrack/ModalToneSection.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import ToggleIconBtn from '../ToggleIconBtn';
 import ModalToneBtn from './ModalToneBtn';
 
 function ModalToneSection({
@@ -6,22 +8,39 @@ function ModalToneSection({
   toggleSelected,
   useFlats,
 }) {
+  const [minimized, setMinimized] = useState(false);
+
+  const toggleMinimize = () => {
+    setMinimized((prev) => !prev);
+  };
   const { displayName, options } = choice;
 
   return (
     <div className="modal-tone-section">
-      <p>{displayName}</p>
-      <div className="modal-tone-section-btns">
-        {options.map((o) => (
-          <ModalToneBtn
-            key={o.name}
-            option={o}
-            selectedTones={selectedTones}
-            toggleSelected={toggleSelected}
-            useFlats={useFlats}
-          />
-        ))}
-      </div>
+      <p className="modal-tone-section-text">
+        {displayName}{' '}
+        <ToggleIconBtn
+          btnClass="btn"
+          btnFunc={toggleMinimize}
+          handleMouseEnter={null}
+          conditional={minimized}
+          icon1="add-icon"
+          icon2="minus-icon"
+        />
+      </p>
+      {!minimized && (
+        <div className="modal-tone-section-btns">
+          {options.map((o) => (
+            <ModalToneBtn
+              key={o.name}
+              option={o}
+              selectedTones={selectedTones}
+              toggleSelected={toggleSelected}
+              useFlats={useFlats}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
